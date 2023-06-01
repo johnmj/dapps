7dae7adc0a787710f7782db34012d0baf48f4067
John Mohan <John_Mohan@epam.com>
2023-06-01 15:35:28 +0200

    Fixed provider error

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+   # access_key = "${var.aws_access_key}"
+   # secret_key = "${var.aws_secret_key}"
+    region = "${var.aws_region}"
+}
 data "aws_ami" "ubuntu_server" {
   most_recent = true
   owners = ["099720109477"]
